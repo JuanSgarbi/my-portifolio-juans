ccd883511809eb5ea763c1a24b8b73a90cfd492d
Juan Sgarbi <juan.sgarbi1@hotmail.com>
2023-03-28 12:19:55 -0300

    finish portifolio

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,9 +1,9 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "nekelpatrick",
-  whatsappNumber: "+5541988819028",
-  emailUser: "samuel@kenzie.com.br",
-  instagramUser: "samuelleaoui",
-  facebookUser: "",
+  nameUser: "Juan Sgarbi",
+  githubUser: "JuanSgarbi",
+  linkedinUser: "juansgarbi",
+  whatsappNumber: "+5512997478524",
+  emailUser: "juan.sgarbi1@hotmail.com",
+  instagramUser: "juansgarbi",
+  facebookUser: "juan.sgarbi",
 };
